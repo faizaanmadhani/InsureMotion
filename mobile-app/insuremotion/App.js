0d2fc0af5d874830343efbbbd64de4c454f4c320
KeshavChawla <keshav1chawla@gmail.com>
2019-08-24 14:03:42 -0400

    Included formula

--- mobile-app/insuremotion/App.js
+++ mobile-app/insuremotion/App.js
@@ -8,13 +8,42 @@ import { Alert } from 'react-native'
 export default function App() {
 
   ShakeEventExpo.addListener(() => {
-    Alert.alert('Shaking!!!');
-    fetch ('')
+    Alert.alert('SHAKING!!!');
+  /*  fetch('', {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    firstParam: 'roadRage',
+    secondParam: 'Distraction',
+  }), */
+
+  // Include function to get data to pass to Function
+
+  function detRates(roadRage, distraction) {
+    if (roadRage > 0.8 && distraction > 0.8) {
+      return "Your Insurance Rates will likely see no decrease in value"
+    } else if (roadRage > 0.6 && distraction > 0.6) {
+      return "Your Insurance Rates will likely see little decrease in value (Expect a 0-1% decrease)"
+    } else if (roadRage > 0.6 || distraction > 0.6) {
+      return "Your Insurance Rates will likely see little decrease in value (Expect a 1-2% decrease)"
+    } else if (roadRage < 0.3 && distraction < 0.3) {
+      return "Your Insurance Rates will likely see a great decrease in value (Expect a 5-7% decrease or more)"
+    } else {
+      return "Your Insurance Rates will likely see a moderate decrease in value (Expect a 4-5% decrease)"
+    }
+
+  }
+  Alert.alert(detRates(1, 0.8));
+
+
    });
-  
+
   return (
     <View style={styles.container}>
-      <Text>InsurEmotion, drive calm and get lower insurance.</Text>
+      <Text>InsurEmotion, drive calm and get lower insurance. </Text>
 
       <AccelerometerSensor />
 
