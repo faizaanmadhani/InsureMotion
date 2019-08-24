9bee6556452d7c7f00ff057459483692611ca6ae
Sophie <sophieevak1990@gmail.com>
2019-08-24 04:24:36 -0400

    Get frames working

--- video.py
+++ video.py
@@ -15,39 +15,27 @@ import request
 
 
 def captureFrame():
-    videoStream = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    frameIndex = 1
+    videoStream = cv2.VideoCapture(0)
 
-    # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
-
-    start = time.time()
+    video_start = time.time()
+    last_frame_time = time.time()
     while videoStream.isOpened():
-        if (time.time() - start) >= 30.0:
+        curr_time = time.time()
+        if (curr_time - video_start) >= 30.0:
             break
+
         ret, frame = videoStream.read()
         if ret:
             cv2.imshow('frame', frame)
-            out.write(frame)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #   break
+            if curr_time - last_frame_time >= 10.0:
+                cv2.imwrite(str(frameIndex) + '.jpeg', frame)
+                # request.req('frame.jpeg')
+                frameIndex += 1
+                last_frame_time = curr_time
 
-    videoStream.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-    videoStream = cv2.VideoCapture('output.mp4')
-    frameIndex = 1
-    last_recorded_time = time.time()
-    while videoStream.isOpened():
-        curr_time = time.time()
-        ret, frame = videoStream.read()
-
-        if ret and (curr_time - last_recorded_time >= 10.0):
-            cv2.imwrite(str(frameIndex) + '.jpeg', frame)
-            # request.req('frame.jpeg')
-            frameIndex += 1
-            last_recorded_time = curr_time
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     videoStream.release()
     cv2.destroyAllWindows()
